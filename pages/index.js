f76ab31c6fc7190357c0cd6033e68104ef093aca
Ana Karla Thomaz <140630544+anathomaz2@users.noreply.github.com>
2025-09-17 02:07:52 +0000

    fix na palavra saudades

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1> Mesmo você não acreditando, to morrendo de suadades amor </h1>;
+  return <h1> Mesmo você não acreditando, to morrendo de saudades amor </h1>;
 }
 
 export default Home;
